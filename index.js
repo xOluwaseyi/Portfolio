a179b32823fbceda385bd2425074b0c30ac66e24
Oluwaseyi F <75592533+xOluwaseyi@users.noreply.github.com>
2021-12-19 21:17:41 +0100

    Added loader

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ const navUL = document.getElementById("nav-ul")
 hamburger.addEventListener("click", () => {
     navUL.classList.toggle("show")
 })
+
+var load = document.getElementById("loading");
+
+    function loader(){
+      load.style.display = "none"
+    }
