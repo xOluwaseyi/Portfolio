f4759fcdf39f6334bb16c3add8f6c9f395ece6f9
Fagbemi Oluwaseyi <seyifagbemi211@gmail.com>
2022-01-19 18:16:00 +0100

    made few changes

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const hamburger = document.getElementById("hamburger")
-const navUL = document.getElementById("nav-ul")
+const hamburger = document.getElementById("hamburger");
+const navUL = document.getElementById("nav-ul");
 
 hamburger.addEventListener("click", () => {
-    navUL.classList.toggle("show")
-})
+  navUL.classList.toggle("show");
+});
 
 var load = document.getElementById("loading");
 
-    function loader(){
-      load.style.display = "none"
-    }
+function loader() {
+  load.style.display = "none";
+}
